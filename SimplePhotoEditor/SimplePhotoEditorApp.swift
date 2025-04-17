92ffef83809fcca67fccf8225940e6a2b92267ca
Ivan Korobov <ikorobov.dev@gmail.com>
2025-04-18 00:23:22 +0200

    Initial Commit

---
+++ SimplePhotoEditor/SimplePhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePhotoEditorApp.swift
+//  SimplePhotoEditor
+//
+//  Created by ikorobov on 18. 4. 2025..
+//
+
+import SwiftUI
+
+@main
+struct SimplePhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
